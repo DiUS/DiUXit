b82cdcfac0930b434f1ad4b9bced31620bde10e0
Mario Martinez <zenitram.oiram@gmail.com>
2016-09-11 11:47:47 +1000

    Integrate basic chat app

--- server/index.js
+++ server/index.js
@@ -1,12 +1,46 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-var port = process.env.PORT || 7777;
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const users = [];
+const connections = [];
+
+console.log('Server running');
+
+server.listen(process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+io.sockets.on('connection', socket => {
+  connections.push(socket);
+  console.log(`Connected: ${connections.length} sockets connected`);
+
+  // Disconnect
+  socket.on('disconnect', () => {
+    users.splice(users.indexOf(socket.username), 1);
+    connections.splice(connections.indexOf(socket), 1);
+    console.log(`Disconnect: ${connections.length} sockets connected`);
+  });
+
+  // Send Message
+  socket.on('send message', data => {
+    io.sockets.emit('new message', {msg: data, user: socket.username});
+  });
+
+  // New User
+  socket.on('new user', (data, callback) => {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames() {
+    io.sockets.emit('get users', users);
+  }
 });
